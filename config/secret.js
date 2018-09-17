682d807ebb0a89965cd77ae63c29f95d78ec9cb0
kris <wavesuav@outlook.com>
2018-09-17 17:43:15 +0800

    first commit

--- config/secret.js
+++ config/secret.js
@@ -1,6 +1,6 @@
 module.exports = {
  database: process.env.DATADASE || 'mongodb://localhost:27017/hmongtraveling',
  port: process.env.PORT || 3000,
- secret: process.env.SECRET || 'hmongtravelling223344556677889900',
- jwt_key:process.env.JWT_KEY || 'hmongtravelling223344jwts'
+ secret: process.env.SECRET || '223344556677889900',
+ jwt_key:process.env.JWT_KEY || '223344jwts'
 }
